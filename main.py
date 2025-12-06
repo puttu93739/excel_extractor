55eee8d707f73c7882050b76333cd11a42929eed
puttu93739 <hangalmathputtaraj@gmail.com>
2025-12-06 16:06:10 +0530

    excel-123:adding saving txt api

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from extractor import ExcelExtractor
 
 
+def save_results_to_text(counter, output_file="results.txt"):
+    with open(output_file, "w") as f:
+        f.write("Group Name\t| Occurrences\n")
+        f.write("---------------------------------\n")
+
+        for group, count in counter.items():
+            f.write(f"{group}\t{count}\n")
+
+    print(f"Results saved to {output_file}")
+
 def main():
     file_path = f"coding_challenge_test.xlsx"
     column_name = "Additional comments"
@@ -8,6 +18,8 @@ def main():
 
     extractor = ExcelExtractor(tag)
     result = extractor.extract_from_file(file_path, column_name)
+
+    save_results_to_text(result, "group_output.txt")
 
     print("\nGroup Name\t\tOccurrences")
     for group, count in result.items():
